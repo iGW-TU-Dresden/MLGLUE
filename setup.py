0258bf4c2d8e410c7596c6ccfd0d6eae4009f5e7
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2023-10-27 09:56:52 +0200

    update for publishing

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MLGLUE",
+    author = "M. G. Rudolph",
+    version = "0.0.1",
+    description = "a Python implementation of the (M)ulti(l)evel (G)eneralized (L)ikelihood (E)stimation (MLGLUE) algorithm and utility functions",
+    url = "https://github.com/iGW-TU-Dresden/MLGLUE",
+    author_email = "max_gustav.rudolph@tu-dresden.de",
+    license = "MIT",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Hydrology"
+        ],
+    platforms = "Windows",
+    install_requires = [
+        "numpy>=1.23.5",
+        "matplotlib>=3.6.3",
+        "ray>=2.2.0"
+        ],
+    packages = find_packages(exclude=[])
+    )
