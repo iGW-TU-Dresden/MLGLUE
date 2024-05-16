ed51e7b1f8960771db3271c4048091fbec7b0278
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2024-05-16 18:59:24 +0200

    update rtd docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,13 @@ release = '0.0.5'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+'sphinx.ext.todo',
+'sphinx.ext.viewcode',
+'sphinx.ext.autodoc',
+'sphinx.ext.napoleon',
+'sphinx_wagtail_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -29,5 +35,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_wagtail_theme'
 html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -3,16 +3,16 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to MLGLUE's documentation!
+Welcome to the documentation of the MLGLUE Python package!
 ==================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 3
    :caption: Contents:
 
    modules
 
-Indices and tables
+Indices and Tables
 ==================
 
 * :ref:`genindex`
