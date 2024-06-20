618ff216b0b5d09bd6263fca7befecca73159ed1
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2024-06-19 20:07:53 -0700

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -8,13 +8,18 @@
 
 import os
 import sys
+from datetime import date
+
+from MLGLUE import __version__
 
 sys.path.insert(0, os.path.abspath(".."))
 
 project = 'MLGLUE'
 copyright = '2024, Max Rudolph'
 author = 'Max Rudolph'
-release = '0.0.5'
+release = __version__
+version = __version__
+year = date.today().strftime("%Y")
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -37,3 +42,28 @@ autodoc_member_order = 'bysource'
 
 html_theme = 'furo'
 html_static_path = ['_static']
+html_logo = "_static/MLGLUE_logo.png"
+html_short_title = "MLGLUE"
+html_show_sphinx = True
+html_show_copyright = True
+
+html_theme_options = {
+    "use_edit_page_button": True,
+    "header_links_before_dropdown": 6,
+    "icon_links": [
+        {
+            "name": "GitHub",  # Label for this link
+            "url": "https://github.com/iGW-TU-Dresden/MLGLUE",  # required
+            "icon": "fab fa-github-square",
+            "type": "fontawesome",  # Default is fontawesome
+        }
+    ],
+}
+
+
+html_context = {
+    "github_user": "iGW-TU-Dresden",
+    "github_repo": "MLGLUE",
+    "github_version": "main",
+    "doc_path": "docs",
+}
