8b4014dd107bd3bc50163913b870b6466f75996b
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2024-12-04 10:53:36 +0100

    Update __init__.py

--- MLGLUE/__init__.py
+++ MLGLUE/__init__.py
@@ -1,4 +1,4 @@
-from .MLGLUE import MLGLUE, MLGLUEWorker
+from .MLGLUE import MLGLUE, MLGLUELocalWorker, MLGLUERemoteWorker
 from .Likelihoods import InverseErrorVarianceLikelihood, \
     RelativeVarianceLikelihood, GaussianLogLikelihood
 from .version import __version__
